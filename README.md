99e0f5b5fd67c361dbe543c95a252a31ceef76ca
erickbmr <erickbrener369@gmail.com>
2020-06-05 17:01:50 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cpf-validate
-Test to validate a Brazilian CPF (Cadastro de pessoa física)
+Test to validate a Brazilian CPF (Cadastro de pessoa física) in [Julia](https://julialang.org/)
---
+++ cpf-validate.jl
@@ -0,0 +1,111 @@
+function sum(counter, final, cpfArray)
+    sum = 0
+    index = 1
+
+    #sum the numbers * counter for verification
+    while counter >= final
+
+        #Example: CPF = 555.555.555-55
+        # First Time: (10 * 5) + (9 * 5) + (8 * 5) ...
+        # Second Time: (11 * 5) + (10 * 5) + (9 * 5) ...
+        value = counter * cpfArray[index]
+        sum += value
+        counter -= 1
+        index += 1
+    
+    end
+
+    return sum
+end
+
+function intToArray(number)
+
+    #array to cpf, each index is a number of cpf
+    cpfArray = []
+
+    #to separate numbers by divisions
+    divider = 10000000000
+
+    #transform int to array
+    #first division out of loop to set remainder value
+    value = div(number, divider)
+    remainder = number % divider
+    append!(cpfArray, value)
+    divider = div(divider, 10)
+
+    while divider >= 1
+
+        value = div(remainder, divider)
+        append!(cpfArray, trunc(Int, value))
+        remainder = remainder % divider
+        divider = div(divider, 10)
+
+    end
+
+    return cpfArray
+end
+
+function validate(number)
+
+    if isa(number, Int64)
+
+        cpfArray = intToArray(number)
+
+        #number to make multiplications and also index of the digit
+        count = 10
+        
+        #first sum of validation process
+        firstSum = sum(count, 2, cpfArray)
+
+        #first verification of digit
+        #round up remainder and transform to int
+        remainder = trunc(Int, round(firstSum % 11))
+
+        #expected to be the first digit
+        firstDigit = 11 - remainder
+
+        if firstDigit < 0
+            return false
+        end
+
+        if firstDigit > 9
+            if cpfArray[count] != 0
+                return false
+            end
+        end
+
+        if firstDigit != cpfArray[count]
+            return false
+        end
+        
+        #second sum of validation process
+        secondSum = sum(count + 1, 2, cpfArray)
+
+        #second verification of digit
+        remainder = trunc(Int, round(secondSum % 11))
+
+        #expected to be the second digit
+        secondDigit = 11 - remainder
+
+        if secondDigit < 0
+            return false
+        end
+
+        if secondDigit > 9
+            if cpfArray[count + 1] != 0
+                return false
+            end
+        end
+
+        if secondDigit != cpfArray[count + 1]
+            return false
+        end
+
+        #all operationd were performed and no invalidation occurred
+        return true
+
+    else
+        return false
+    end
+
+end
